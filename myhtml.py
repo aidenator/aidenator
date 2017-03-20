b7fc91992de8ac2c3099674a398f343d3c98a05f
Aiden Hoopes <aidenator00@gmail.com>
2017-03-19 20:49:44 -0700

    Added a favicon and a custom CSS file.

--- myhtml.py
+++ myhtml.py
@@ -6,15 +6,16 @@ header ="""
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="icon" href="../static/img/favicon.ico">
+        <link rel="shortcut icon" href="http://www.aidenator.com/static/img/favicon.ico">
 
         <title>Aidenator</title>
 
         <!-- Bootstrap core CSS -->
         <link href="../static/css/bootstrap.min.css" rel="stylesheet">
-
         <!-- Custom styles for this template -->
         <link href="../static/css/narrow-jumbotron.css" rel="stylesheet">
+        <!-- Aiden's custom CSS changes -->
+        <link href="../static/css/my-changes.css" rel="stylesheet">
     </head>
 
     <body>
@@ -24,17 +25,42 @@ header ="""
                 <nav>
                     <ul class="nav nav-pills float-right">
                         <li class="nav-item">
-                            <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                           <a class="nav-link" href="#" data-toggle="modal" data-target="#aboutModal">About</a>
+
+                           <!-- Modal -->
+                           <div class="modal fade" id="aboutModal" tabindex="-1" role="dialog" aria-labelledby="aboutModalLabel">
+                             <div class="modal-dialog modal-md" role="document">
+                               <div class="modal-content">
+                                 <div class="modal-header">
+                                   <h5 class="modal-title" id="aboutModalLabel">About</h5>
+                                 </div>
+                                 <div class="modal-body">
+                                   This site is purely non-profit. I swear I'm not paying the CIA to host this...
+                                 </div>
+                               </div>
+                             </div>
+                           </div>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                           <a class="nav-link" href="#" data-toggle="modal" data-target="#contactModal">Contact</a>
+
+                           <!-- Modal -->
+                           <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="contactModalLabel">
+                             <div class="modal-dialog modal-md" role="document">
+                               <div class="modal-content">
+                                 <div class="modal-header">
+                                   <h5 class="modal-title" id="contactModalLabel">Contact</h5>
+                                 </div>
+                                 <div class="modal-body">
+                                   Use this site to see if I'm home and then just contact me in person. If not, just text me. :^P
+                                 </div>
+                               </div>
+                             </div>
+                           </div>
                         </li>
                     </ul>
                 </nav>
-                <h3 class="text-muted">Aidenator.com</h3>
+                <h3 class="text-muted"><a href="/" class="text-muted">Aidenator.com</a></h3>
             </div>
 """
 
